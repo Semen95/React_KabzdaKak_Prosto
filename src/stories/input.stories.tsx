cf31d29984a7dca7b68dbcbf59d87a508dc226d0
Semen95 <semaforever1995@gmail.com>
2022-09-13 18:21:10 +0300

    13_ControlledInput, checkbox, select

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -1,4 +1,5 @@
 import React, {ChangeEvent, useRef, useState} from 'react';
+import {action} from "@storybook/addon-actions";
 
 export default {
     title: "input",
@@ -9,7 +10,7 @@ export const UnconntrolledInput = () => <input/>
 
 export const TrackValueOfUnconntrolledInput = () => {
     const [value, setValue] = useState("")
-    const onChange = (event:ChangeEvent<HTMLInputElement>) => {
+    const onChange = (event: ChangeEvent<HTMLInputElement>) => {
         const actualValue = event.currentTarget.value;
         setValue(actualValue);
     }
@@ -35,6 +36,35 @@ export const GetValueOfOfUnconntrolledInputButtonPress = () => {
         <button onClick={save}>save</button>
         - actual value:{value}
     </>
+}
+
+export const ControlledInput = () => {
+    const [parentValue, setParentValue] = useState("")
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setParentValue(e.currentTarget.value)
+    }
+    return <input value={parentValue} onChange={onChange}/>
+}
+
+export const ControlledCheckbox = () => {
+    const [parentValue, setParentValue] = useState(true)
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setParentValue(e.currentTarget.checked)
+    }
+    return <input type="checkbox" checked={parentValue} onChange={onChange}/>
+}
+
+export const ControlledSelect = () => {
+    const [parentValue, setParentValue] = useState<string|undefined>(undefined)
+    const onChange = (e: ChangeEvent<HTMLSelectElement>) => {
+        setParentValue(e.currentTarget.value)
+    }
+    return <select value={parentValue} onChange={onChange}>
+        <option>none</option>
+        <option value={"1"}>Minsk</option>
+        <option value={"2"}>Moscow</option>
+        <option value={"3"}>Kiev</option>
+    </select>
 }
 
 export const ControlledInputWithFixedValue = () => <input value={"it-incubator.by"}/>
